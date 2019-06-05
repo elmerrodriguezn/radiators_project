9abb038ec85ae3d52485a01f411db9497a453c18
Elmer Rodriguez <elmer.rodriguezn@gmail.com>
2019-06-05 13:33:28 -0700

    Cleaning code

--- contact/api_queries.py
+++ contact/api_queries.py
@@ -2,18 +2,24 @@ from api_connection import *
 import json
 from django.shortcuts import redirect
 
+# Template function to query api taking two arguments
+api_template = lambda model, operation, query='', fields='' : models.execute_kw(db, uid, password, model, operation,[query], fields)
+
 def create_lead(fullName, email, phone, msg):
-    raw_search= models.execute_kw(
-    db,
-    uid,
-    password,
-   'crm.lead', 'create', [{
-       'name': 'Contacto Radiadores Mesabi',
+    # Template api function parameters
+    model = 'crm.lead'
+    operation = 'create'
+    # Query accepting q parameter requested in the view
+    query = {
+       'name': 'radiadores-mesabi.com.mx',
        'contact_name': fullName,
        'email_from': email,
        'phone': phone,
        'description': msg
-   }])
+       }
+    
+    # Template api function
+    api_template(model, operation, query)
 
     return print('envio exitoso')
     
--- static/js/recaptcha.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-    var onloadCallback = function() {
-        alert("grecaptcha is ready!")
-    };
-</script>
